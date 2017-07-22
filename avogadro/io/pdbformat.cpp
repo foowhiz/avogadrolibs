b85f76aa9607d36c35a64cba699b952a34529c3d
Tanuj Kumar <kmrtnjscnc@gmail.com>
2017-07-22 19:09:45 +0530

    Fixed coding style

--- avogadro/io/pdbformat.cpp
+++ avogadro/io/pdbformat.cpp
@@ -49,33 +49,20 @@ PdbFormat::~PdbFormat()
 
 bool PdbFormat::read(std::istream& in, Core::Molecule& mol)
 {
+  Matrix4f matrix = Matrix4f::Identity(); // Stores one BIOMT or SMTRY matrix
+  vector<Matrix4f> bioMatrices;           // Vector of BIOMT matrices
+  vector<Matrix4f> symMatrices;           // Vector of SMTRY matrices
+
   string buffer;
 
-  while (getline(in, buffer)) {
-    Matrix4f bioMatrix;           // Stores one BIOMT matrix
-    vector<Matrix4f> bioMatrices; // Vector of BIOMT matrices
+  while (getline(in, buffer)) { // Read Each line one by one
 
-    Matrix4f symMatrix;           // Stores one SMTRY matrix
-    vector<Matrix4f> symMatrices; // Vector of SMTRY matrices
-
-    if(startsWith(buffer, "ENDMDL"))
+    if (startsWith(buffer, "ENDMDL"))
       break;
 
-    else if(startsWith(buffer, "ATOM") || startsWith(buffer, "HETATM")) {
-    // 2 spaces after "ATOM" in other
-    // codes, why?
+    else if (startsWith(buffer, "ATOM") || startsWith(buffer, "HETATM")) {
+      // 2 spaces after "ATOM" in other codes, why?
       int serial;
-      string name;
-      char altLoc;    // Alternate location
-      string ResName; // Residue name
-      char chainId;   // Chain identifier
-      int resSeq;     // Residue sequence number
-      char iCode;     // Unique ID for inserted residues
-      Vector3 pos;    // Coordinates
-      float tempFactor;
-      string element; // Element symbol, right justififed
-      string charge;
-
       bool ok(false);
       serial = lexicalCast<int>(buffer.substr(6, 5), ok);
       // PDB columns start numbering at 1
@@ -84,69 +71,76 @@ bool PdbFormat::read(std::istream& in, Core::Molecule& mol)
         return false;
       }
 
+      string name;
       name = buffer.substr(12, 4);
 
+      char altLoc; // Alternate location
       altLoc = lexicalCast<char>(buffer.substr(16, 1), ok);
       if (!ok) {
         appendError("Error parsing alternate location");
         return false;
       }
 
+      string ResName; // Residue name
       ResName = buffer.substr(17, 3);
-      
+
+      char chainId; // Chain identifier
       chainId = lexicalCast<char>(buffer.substr(21, 1), ok);
       if (!ok) {
-        appendError("Error parsing chain Identification number");
+        appendError("Error parsing chain identification number");
         return false;
       }
 
+      int resSeq; // Residue sequence number
       resSeq = lexicalCast<int>(buffer.substr(22, 4), ok);
       if (!ok) {
         appendError("Error parsing residue sequence number");
         return false;
       }
 
+      char iCode; // Unique ID for inserted residues
       iCode = lexicalCast<char>(buffer.substr(26, 1), ok);
-      if(!ok) {
+      if (!ok) {
         appendError("Error parsing ICode");
         return false;
       }
 
+      Vector3 pos; // Coordinates
       pos.x() = lexicalCast<Real>(buffer.substr(30, 8), ok);
       if (!ok) {
-      appendError("Failed to parse x coordinate: " + buffer.substr(30, 8));
-      return false;
+        appendError("Failed to parse x coordinate: " + buffer.substr(30, 8));
+        return false;
       }
 
       pos.y() = lexicalCast<Real>(buffer.substr(38, 8), ok);
       if (!ok) {
-      appendError("Failed to parse y coordinate: " + buffer.substr(38, 8));
-      return false;
+        appendError("Failed to parse y coordinate: " + buffer.substr(38, 8));
+        return false;
       }
 
       pos.z() = lexicalCast<Real>(buffer.substr(46, 8), ok);
       if (!ok) {
-      appendError("Failed to parse z coordinate: " + buffer.substr(46, 8));
-      return false;
+        appendError("Failed to parse z coordinate: " + buffer.substr(46, 8));
+        return false;
       }
 
+      float tempFactor;
       tempFactor = lexicalCast<float>(buffer.substr(60, 6), ok);
       if (!ok) {
         appendError("Error parsing Temp factor");
         return false;
       }
 
+      string element; // Element symbol, right justififed
       element = buffer.substr(76, 2);
+
+      string charge;
       charge = buffer.substr(78, 2);
     }
 
     else if (startsWith(buffer, "CONECT")) {
-      int a;  // Atom serial number
-      int b1; // Serial numbers of subsequent bonded atoms
-      int b2;
-      int b3;
-      int b4;
 
+      int a; // Atom serial number
       bool ok(false);
       a = lexicalCast<int>(buffer.substr(6, 5), ok);
       if (!ok) {
@@ -154,24 +148,28 @@ bool PdbFormat::read(std::istream& in, Core::Molecule& mol)
         return false;
       }
 
+      int b1; // Serial numbers of subsequent bonded atoms
       b1 = lexicalCast<int>(buffer.substr(11, 5), ok);
       if (!ok) {
         appendError("Error parsing serial number of first bonded atom");
         return false;
       }
 
+      int b2;
       b2 = lexicalCast<int>(buffer.substr(16, 5), ok);
       if (!ok) {
         appendError("Error parsing serial number of second bonded atom");
         return false;
       }
 
+      int b3;
       b3 = lexicalCast<int>(buffer.substr(21, 5), ok);
       if (!ok) {
         appendError("Error parsing serial number of third bonded atom");
         return false;
       }
 
+      int b4;
       b4 = lexicalCast<int>(buffer.substr(26, 5), ok);
       if (!ok) {
         appendError("Error parsing serial number of fourth bonded atom");
@@ -180,19 +178,8 @@ bool PdbFormat::read(std::istream& in, Core::Molecule& mol)
     }
 
     else if (startsWith(buffer, "HELIX")) { // Space after HELIX?
-      int serial;         // Serial number of helix, starts at 1
-      string helixId;     // Alphanumeric helix identifier
-      string initResName;
-      char initChainId;
-      int initSeqNum;
-      char initICode;
-      string endResName;
-      char endChainId;
-      int endSeqNum;
-      char endICode;
-      int helixClass;   // Helix type classification
-      int length;       // Length of the helix
 
+      int serial; // Serial number of helix, starts at 1
       bool ok(false);
       serial = lexicalCast<int>(buffer.substr(7, 3), ok);
       if (!ok) {
@@ -200,53 +187,65 @@ bool PdbFormat::read(std::istream& in, Core::Molecule& mol)
         return false;
       }
 
+      string helixId; // Alphanumeric helix identifier
       helixId = buffer.substr(11, 3);
+
+      string initResName;
       initResName = buffer.substr(15, 3);
 
+      char initChainId;
       initChainId = lexicalCast<char>(buffer.substr(19, 1), ok);
       if (!ok) {
         appendError("Error parsing initial chain ID");
         return false;
       }
 
+      int initSeqNum;
       initSeqNum = lexicalCast<int>(buffer.substr(21, 4), ok);
       if (!ok) {
         appendError("Error parsing initial sequence number");
         return false;
       }
 
+      char initICode;
       initICode = lexicalCast<char>(buffer.substr(25, 1), ok);
       if (!ok) {
         appendError("Error parsing initial ICode");
         return false;
       }
 
+      string endResName;
       endResName = buffer.substr(27, 3);
 
+      char endChainId;
       endChainId = lexicalCast<char>(buffer.substr(31, 1), ok);
       if (!ok) {
         appendError("Error parsing end chain ID");
         return false;
       }
 
+      int endSeqNum;
       endSeqNum = lexicalCast<int>(buffer.substr(33, 4), ok);
       if (!ok) {
         appendError("Error parsing end sequence number");
         return false;
       }
 
+      char endICode;
       endICode = lexicalCast<char>(buffer.substr(37, 1), ok);
       if (!ok) {
         appendError("Error parsing end ICode");
         return false;
       }
 
+      int helixClass; // Helix type classification
       helixClass = lexicalCast<int>(buffer.substr(38, 2), ok);
       if (!ok) {
         appendError("Error parsing helix class");
         return false;
       }
 
+      int length; // Length of the helix
       length = lexicalCast<int>(buffer.substr(71, 5), ok);
       if (!ok) {
         appendError("Error parsing helix length");
@@ -255,29 +254,8 @@ bool PdbFormat::read(std::istream& in, Core::Molecule& mol)
     }
 
     else if (startsWith(buffer, "SHEET")) { // Space after SHEET?
-      int strand;
-      string sheetId;
-      int numStrands;
-      string initResName;
-      char initChainId;
-      int initSeqNum;
-      char initICode;
-      string endResName;
-      char endChainId;
-      int endSeqNum;
-      char endICode;
-      int sense;        // Sense of strand wrt previous strand, -1, 0 or 1
-      string curAtom;   // Cur refers to current strand
-      string curResName;
-      char curChainId;
-      int curResSeq;
-      char curICode;
-      string prevAtom;
-      string prevResName;
-      char prevChainId;
-      int prevResSeq;
-      char prevICode;
 
+      int strand;
       bool ok(false);
       strand = lexicalCast<int>(buffer.substr(7, 3), ok);
       if (!ok) {
@@ -285,96 +263,119 @@ bool PdbFormat::read(std::istream& in, Core::Molecule& mol)
         return false;
       }
 
+      string sheetId;
       sheetId = buffer.substr(11, 3);
-      
+
+      int numStrands;
       numStrands = lexicalCast<int>(buffer.substr(14, 2), ok);
       if (!ok) {
         appendError("Error parsing number of strands");
         return false;
       }
 
+      string initResName;
       initResName = buffer.substr(17, 3);
-      
+
+      char initChainId;
       initChainId = lexicalCast<char>(buffer.substr(21, 1), ok);
       if (!ok) {
         appendError("Error parsing end initial chain ID");
         return false;
       }
 
+      int initSeqNum;
       initSeqNum = lexicalCast<int>(buffer.substr(22, 4), ok);
       if (!ok) {
         appendError("Error parsing initial sequence number");
         return false;
       }
 
+      char initICode;
       initICode = lexicalCast<char>(buffer.substr(26, 1), ok);
       if (!ok) {
         appendError("Error parsing initial ICode");
         return false;
       }
 
+      string endResName;
       endResName = buffer.substr(28, 3);
 
+      char endChainId;
       endChainId = lexicalCast<char>(buffer.substr(32, 1), ok);
       if (!ok) {
         appendError("Error parsing end chain ID");
         return false;
       }
 
+      int endSeqNum;
       endSeqNum = lexicalCast<int>(buffer.substr(33, 4), ok);
       if (!ok) {
         appendError("Error parsing end sequence number");
         return false;
       }
 
+      char endICode;
       endICode = lexicalCast<char>(buffer.substr(37, 1), ok);
       if (!ok) {
         appendError("Error parsing end ICode");
         return false;
       }
 
+      int sense; // Sense of strand wrt previous strand, -1, 0 or 1
       sense = lexicalCast<int>(buffer.substr(38, 2), ok);
       if (!ok) {
         appendError("Error parsing sense");
         return false;
       }
 
+      string curAtom;
       curAtom = buffer.substr(41, 4);
+
+      string curResName;
       curResName = buffer.substr(45, 3);
 
+      char curChainId;
       curChainId = lexicalCast<char>(buffer.substr(49, 1), ok);
       if (!ok) {
         appendError("Error parsing chain ID");
         return false;
       }
 
+      int curResSeq;
       curResSeq = lexicalCast<int>(buffer.substr(50, 4), ok);
       if (!ok) {
         appendError("Error parsing current residue sequence");
         return false;
       }
 
+      char curICode;
       curICode = lexicalCast<char>(buffer.substr(54, 1), ok);
       if (!ok) {
         appendError("Error parsing current ICode");
         return false;
       }
 
+      string prevAtom;
       prevAtom = buffer.substr(56, 4);
+
+      string prevResName;
       prevResName = buffer.substr(60, 3);
 
+      char prevChainId;
       prevChainId = lexicalCast<char>(buffer.substr(64, 1), ok);
       if (!ok) {
         appendError("Error parsing previous chain ID");
         return false;
       }
 
+      int prevResSeq;
       prevResSeq = lexicalCast<int>(buffer.substr(65, 4), ok);
       if (!ok) {
         appendError("Error parsing previous residue sequence");
         return false;
       }
 
+      char prevICode;
       prevICode = lexicalCast<char>(buffer.substr(69, 1), ok);
       if (!ok) {
         appendError("Error parsing previous ICode");
@@ -382,84 +383,80 @@ bool PdbFormat::read(std::istream& in, Core::Molecule& mol)
       }
     }
 
-    else if(startsWith(buffer, "REMARK 350   BIOMT")) {
+    else if (startsWith(buffer, "REMARK 350   BIOMT")) {
       // Further possible optimization by reading all matrices together
       bool ok(false);
-      int row = lexicalCast<int>(buffer.substr(18, 1), ok) - 1;
+      int row;
+      row = lexicalCast<int>(buffer.substr(18, 1), ok) - 1;
       if (!ok) {
         appendError("Error parsing BIOMT matrix row number");
         return false;
       }
 
-      bioMatrix(row, 0) = lexicalCast<float>(buffer.substr(23, 10), ok);
+      matrix(row, 0) = lexicalCast<float>(buffer.substr(23, 10), ok);
       if (!ok) {
         appendError("Error parsing BIOMT matrix column 0");
         return false;
       }
 
-
-      bioMatrix(row, 1) = lexicalCast<float>(buffer.substr(33, 10), ok);
+      matrix(row, 1) = lexicalCast<float>(buffer.substr(33, 10), ok);
       if (!ok) {
         appendError("Error parsing BIOMT matrix column 1");
         return false;
       }
 
-
-      bioMatrix(row, 2) = lexicalCast<float>(buffer.substr(43, 10), ok);
+      matrix(row, 2) = lexicalCast<float>(buffer.substr(43, 10), ok);
       if (!ok) {
         appendError("Error parsing BIOMT matrix column 2");
         return false;
       }
 
-
-      bioMatrix(row, 3) = lexicalCast<float>(buffer.substr(53, 5), ok);
+      matrix(row, 3) = lexicalCast<float>(buffer.substr(53, 5), ok);
       if (!ok) {
         appendError("Error parsing BIOMT matrix column 3");
         return false;
       }
 
       if (row == 2)
-        bioMatrices.push_back(bioMatrix);
+        bioMatrices.push_back(matrix);
     }
 
-    else if(startsWith(buffer, "REMARK 290   SMTRY")) {
+    else if (startsWith(buffer, "REMARK 290   SMTRY")) {
       // Further possible optimization by reading all matrices together
       bool ok(false);
-      int row = lexicalCast<int>(buffer.substr(18, 1), ok) - 1;
+      int row;
+      row = lexicalCast<int>(buffer.substr(18, 1), ok) - 1;
       if (!ok) {
         appendError("Error parsing SMTRY matrix row number");
         return false;
       }
 
-      symMatrix(row, 0) = lexicalCast<float>(buffer.substr(23, 10), ok);
+      matrix(row, 0) = lexicalCast<float>(buffer.substr(23, 10), ok);
       if (!ok) {
         appendError("Error parsing SMTRY matrix column 0");
         return false;
       }
 
-
-      symMatrix(row, 1) = lexicalCast<float>(buffer.substr(33, 10), ok);
+      matrix(row, 1) = lexicalCast<float>(buffer.substr(33, 10), ok);
       if (!ok) {
         appendError("Error parsing SMTRY matrix column 1");
         return false;
       }
 
-
-      symMatrix(row, 2) = lexicalCast<float>(buffer.substr(43, 10), ok);
+      matrix(row, 2) = lexicalCast<float>(buffer.substr(43, 10), ok);
       if (!ok) {
         appendError("Error parsing SMTRY matrix column 2");
         return false;
       }
 
-
-      symMatrix(row, 3) = lexicalCast<float>(buffer.substr(53, 5), ok);
+      matrix(row, 3) = lexicalCast<float>(buffer.substr(53, 5), ok);
       if (!ok) {
         appendError("Error parsing SMTRY matrix column 3");
         return false;
       }
 
       if (row == 2)
-        symMatrices.push_back(symMatrix);
+        symMatrices.push_back(matrix);
     }
   }
   return true;
