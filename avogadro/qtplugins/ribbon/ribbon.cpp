79c3b7ba42e1a26dbf909fe63b3df4bb22fd065a
Tanuj Kumar <kmrtnjscnc@gmail.com>
2017-07-31 00:15:07 +0530

    Made planes more accurate

--- avogadro/qtplugins/ribbon/ribbon.cpp
+++ avogadro/qtplugins/ribbon/ribbon.cpp
@@ -27,6 +27,8 @@
 
 #include <Eigen/Dense>
 
+#include <cmath>
+
 #include <QtWidgets/QCheckBox>
 #include <QtWidgets/QDoubleSpinBox>
 #include <QtWidgets/QHBoxLayout>
@@ -57,54 +59,87 @@ Ribbon::~Ribbon()
 
 void Ribbon::process(const Molecule& molecule, Rendering::GroupNode& node)
 {
+  Vector3f temp1;
+  temp1 << 0, 0, 0;
+  Vector3f temp2;
+  temp2 << 0, 0, 0;
+  Vector3f tempc;
+  tempc<< 0, 0, 0;
+
   for(size_t i = 0; i < (molecule.atomCount()-2); i+=2)
   {
-    //size_t i = 0;
-    Core::Atom ca1 = molecule.atom(i);  // First alpha carbon
-    Core::Atom ca2 = molecule.atom(i+2);  // Second alpha carbon
-    Core::Atom o = molecule.atom(i+1);  //Carbonyl oxygen between ca1 & ca2
-    Vector3f a = ca2.position3d().cast<float>() - ca1.position3d().cast<float>();
-    Vector3f b = o.position3d().cast<float>() - ca1.position3d().cast<float>();
-    Vector3f c;
+    Vector3f ca1 = molecule.atom(i).position3d().cast<float>();  // First alpha carbon
+    Vector3f ca2 = molecule.atom(i+2).position3d().cast<float>();  // Second alpha carbn
+    Vector3f o = molecule.atom(i+1).position3d().cast<float>();  //Carbonyl oxygen between ca1 & ca2
+    Vector3f a = ca2 - ca1; // In the direction of the ribbon
+    Vector3f b = o - ca1;
+    Vector3f c; // Normal to peptide plane
 
     //Cross product
     c(0, 0) = a(1, 0)*b(2, 0) - a(2, 0)*b(1, 0);
     c(1, 0) = a(2, 0)*b(0, 0) - a(0, 0)*b(2, 0);
     c(2, 0) = a(0, 0)*b(1, 0) - a(1, 0)*b(0, 0);
+    
+    c = c/(sqrt(pow(c(0, 0), 2) + pow(c(1, 0), 2) + pow(c(2, 0), 2)));  //Normalization
     /*
-     *Need to Normalize
+     *NEED TO USE BETTER FUNCTIONS, PROBABLY FROM EIGEN
     */
 
-    Eigen::Matrix<Real, 3, 1> d;
+    Vector3f d;  // Perpendicular to vector a in peptide plane
     d(0, 0) = c(1, 0)*a(2, 0) - c(2, 0)*a(1, 0);
     d(1, 0) = c(2, 0)*a(0, 0) - c(0, 0)*a(2, 0);
     d(2, 0) = c(0, 0)*a(1, 0) - c(1, 0)*a(0, 0);
+    d = d/(sqrt(pow(d(0, 0), 2) + pow(d(1, 0), 2) + pow(d(2, 0), 2)));  //Normalization
     /*
-     *Need to Normalize
+     *NEED TO USE BETTER FUNCTIONS
     */
 
-    Core::Array<Vector3f> vertices;
-    vertices.push_back(ca1.position3d().cast<float>());
-    vertices.push_back(o.position3d().cast<float>());
-    vertices.push_back(ca2.position3d().cast<float>());
+    Vector3f p = (ca1 + ca2)/2; // Midpoint of ca1 and ca2
+    /*
+     *Need to Translate by 1.5 Angstroms for reasonable helix diameter
+     *in the direction of vector c, if residue in helix
+    */
 
-    Core::Array<Vector3f> normals;  // Should be a good approx
-    for (int j = 0; j < 3; ++j)
+    d = d*3/2;  // d to be multiplied by half of required ribbon width
+    // TODO: Use variable instead of 3
+
+    Vector3f p1 = p - d; // p1 and p2 are end points of ribbon width
+    Vector3f p2 = p + d;
+
+    if( i != 0){
+      Core::Array<Vector3f> vertices;
+      vertices.push_back(temp1);
+      vertices.push_back(temp2);
+      vertices.push_back(p1);
+      vertices.push_back(p2);
+
+      Core::Array<Vector3f> normals;  // Should be a good approx of vertex normal
+      normals.push_back(tempc);
+      normals.push_back(tempc);
+      normals.push_back(c);
       normals.push_back(c);
 
-    unsigned char opacity = 255;
+      unsigned char opacity = 255;
 
-    GeometryNode* geometry = new GeometryNode;
-    node.addChild(geometry);
+      GeometryNode* geometry = new GeometryNode;
+      node.addChild(geometry);
 
-    MeshGeometry* mesh1 = new MeshGeometry;
-    geometry->addDrawable(mesh1);
-    mesh1->setColor(Vector3ub(255, 0, 0));
-    mesh1->setOpacity(opacity);
-    unsigned int index1 = mesh1->addVertices(vertices, normals);
-    mesh1->addTriangle(index1, index1 + 1, index1 + 2);
-    //mesh1->setRenderPass(opacity == 255 ? Rendering::OpaquePass
-      //                                  : Rendering::TranslucentPass);
+      MeshGeometry* mesh1 = new MeshGeometry;
+      geometry->addDrawable(mesh1);
+      mesh1->setColor(Vector3ub(255, 0, 0));
+      mesh1->setOpacity(opacity);
+      unsigned int index1 = mesh1->addVertices(vertices, normals);
+      Core::Array<unsigned int> indices;
+      for(int k = 0; k < 4; ++k)
+        indices.push_back(index1 + k);
+      mesh1->addTriangles(indices);
+      //mesh1->setRenderPass(opacity == 255 ? Rendering::OpaquePass
+        //                                  : Rendering::TranslucentPass);
+    }
+
+    temp1 = p1;
+    temp2 = p2;
+    tempc = c;
   }
 }
 
